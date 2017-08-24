a222a47cf5ef140eb246ad601da9d4d999fdac00
brian d foy <brian.d.foy@gmail.com>
2017-08-24 16:57:25 -0400

    Bump copyright date

--- lib/HTTP/Cookies/Safari.pm
+++ lib/HTTP/Cookies/Safari.pm
@@ -47,7 +47,7 @@ Jon Orwant pointed out the problem with dates too far in the future
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2003-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2003-2017, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
